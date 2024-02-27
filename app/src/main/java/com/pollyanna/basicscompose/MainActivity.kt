369fa910b08e05624ae58f35d5d8a62db6c55e62
Pollyanna Wu <pollyanna.wu@gogolook.com>
2024-02-27 16:42:27 +0800

    Feat: finish lab

--- app/src/main/java/com/pollyanna/basicscompose/MainActivity.kt
+++ app/src/main/java/com/pollyanna/basicscompose/MainActivity.kt
@@ -3,19 +3,31 @@ package com.pollyanna.basicscompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.pollyanna.basicscompose.ui.theme.BasicsCodelabTheme
@@ -33,10 +45,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(
+    modifier: Modifier = Modifier,
+    name: String
+) {
 
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if(expanded.value) 24.dp else 0.dp
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        targetValue = if (expanded) 48.dp else 0.dp,
+        label = "",
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
+
 
     // surface helps us to choose suitable color for text automatically
     Surface(
@@ -47,32 +71,81 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello")
                 Text(
-                    text = "$name"
+                    text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
                 )
             }
             ElevatedButton(onClick = {
-                expanded.value = !expanded.value
+                expanded = !expanded
             }) {
-                Text(if (expanded.value) "Show less" else "Show more")
+                Text(if (expanded) "Show less" else "Show more")
             }
         }
     }
 }
 
 @Composable
-fun MyApp(
+fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
-) {
-
-    Column(modifier = modifier.padding(4.dp)) {
-        for (name in names) {
+    names: List<String> = List(1000){ "$it"}
+){
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
         }
+    }
+
+}
+
+
+@Composable
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World")
+) {
+    // use rememberSaveable to save each state surviving configuration changes (such as rotations) and process death.
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    Column(modifier = modifier.padding(4.dp)) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(
+                onContinueClicked = { shouldShowOnboarding = false }
+            )
+        } else {
+            Greetings()
+        }
+
+    }
+
+}
+
+@Composable
+fun OnboardingScreen(
+    modifier: Modifier = Modifier,
+    onContinueClicked: () -> Unit = {}
+) {
+    // This state should be hoisted to avoid duplicating state
+    // By: This is a property delegate that saves you from typing .value every time.
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(24.dp),
+            onClick = { onContinueClicked() }
+        ) {
+            Text("Continue")
+        }
+
     }
 
 }
